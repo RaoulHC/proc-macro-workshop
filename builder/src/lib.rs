c4532e0e1a6d7f7994fa6e136f65ebfb75e56c0d
Raoul Hidalgo Charman <raoul.hidalgo.charman@gmail.com>
2019-06-23 09:24:19 +0100

    First 3 tasks

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,75 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, parse_macro_input, DeriveInput, Ident, Fields};
+use proc_macro2::Span;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let id = ast.ident;
+
+    let fields = match ast.data {
+        Data::Struct(data) => data.fields,
+        _ => panic!("Can't derive builder that isn't a struct"),
+    };
+    let named_fields = match fields {
+        Fields::Named(x) => x.named,
+        _ => panic!("can't derive builder for struct without named fields"),
+    };
+    let empty_fields = named_fields.iter().map(|x| {
+        let name = &x.ident;
+        quote!{#name: None}
+    });
+
+    let builder_name = Ident::new(&format!("{}Builder", id), Span::call_site());
+
+    let builder = quote!{
+        impl #id {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#empty_fields),*
+                }
+            }
+        }
+    };
+    let builder_fields = named_fields.iter().map(|x| {
+        let name = &x.ident;
+        let ty = &x.ty;
+        quote!{
+            #name: Option<#ty>
+        }
+    });
+    let builder_struct = quote! {
+        pub struct #builder_name {
+            #(#builder_fields),*
+        }
+    };
+
+    let call_setters = named_fields.iter().map(|x| {
+        let name = &x.ident;
+        let ty = &x.ty;
+        quote!{
+            fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let builder_impl = quote! {
+        impl #builder_name {
+            #(#call_setters)*
+        }
+    };
+
+    let total = quote!{
+        #builder
+        #builder_struct
+        #builder_impl
+    };
+
+    TokenStream::from(total)
 }
